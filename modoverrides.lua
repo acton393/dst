fe2b18a8c37f9b0aa0037d3e4fccdab48697655c
acton393 <zhangxing610321@gmail.com>
2022-04-30 12:35:36 +0800

    add modoverrides.lua

---
+++ modoverrides.lua
@@ -0,0 +1,110 @@
+return {
+    ["workshop-2768642680"]={
+        -- Bestiary  
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=2768642680
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-786556008"]={
+        -- 45 Inventory Slots
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=786556008
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-376333686"]={
+        -- Combined Status
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=376333686
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-727774324"]={
+        -- Craft Pot [DS, ROG, SW, DST]
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=727774324
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-569043634"]={
+        -- Campfire Respawn
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=569043634
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-604761020"]={
+        -- Multi Rocks 持久挖矿 卵石 采矿场 挖矿 联机版
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=604761020
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-1892210190"]={
+        -- [Arknights]Amiya
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=1892210190
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-831523966"]={
+        -- 999堆叠
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=831523966
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-347079953"]={
+        -- Display Food Values
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=347079953
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-661253977"]={
+        -- 灵魂携带物品 - Don't Drop Everything
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=661253977
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-812723897"]={
+        -- Extended Map Icons地图显示更多资源图标
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=812723897
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-375850593"]={
+        -- Extra Equip Slots
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=375850593
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-378160973"]={
+        -- Global Positions
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=378160973
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-375859599"]={
+        -- Health Info
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=375859599
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-501385076"]={
+        -- 快速采集
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=501385076
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-666155465"]={
+        -- Show Me (Origin)
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=666155465
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-362175979"]={
+        -- Wormhole Marks [DST]
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=362175979
+      configuration_options={  },
+      enabled=true 
+    },
+    ["workshop-1837053004"]={
+        -- 晓美焰
+        -- https://steamcommunity.com/sharedfiles/filedetails/?id=1837053004
+      configuration_options={  },
+      enabled=true 
+    }
+  }
